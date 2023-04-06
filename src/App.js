e44143d1fa821341252d03966a34d38865373cda
felype-carvalho <felypecarvalho09@outlook.com.br>
2023-04-06 02:46:30 -0300

    Create navbar and footer components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+// components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 // pages
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
+
+
 
 
 
@@ -12,12 +18,14 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <Navbar />
                 <div className='container'>
                     <Routes>
-                        <Route path="/" element={Home}></Route>
-                        <Route path="/about" element={About}></Route>
+                        <Route path="/" element={<Home />}></Route>
+                        <Route path="/about" element={<About />}></Route>
                     </Routes>
                 </div>
+                <Footer />
             </BrowserRouter>
         </div>
     );
