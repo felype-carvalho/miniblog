a1e54718f413b4a399c991acc8b0c807c2bc79d4
felype-carvalho <felypecarvalho09@outlook.com.br>
2023-04-26 09:29:04 -0300

    Create update post function

--- src/pages/EditPost/EditPost.js
+++ src/pages/EditPost/EditPost.js
@@ -3,7 +3,7 @@ import styles from './EditPost.module.css';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useAuthValue } from '../../context/AuthContext';
-import { useInsertDocument } from '../../hooks/useInsertDocument';
+import { useUpdateDocument } from '../../hooks/useUpdateDocument';
 import { useFetchDocument } from '../../hooks/useFetchDocument';
 
 const EditPost = () => {
@@ -29,7 +29,7 @@ const EditPost = () => {
     }, [post])
 
 
-    const { insertDocument, response } = useInsertDocument("posts");
+    const { updateDocument, response } = useUpdateDocument("posts");
 
     const navigate = useNavigate();
 
@@ -56,17 +56,19 @@ const EditPost = () => {
 
         if (formError) return;
 
-        insertDocument({
+        const data = {
             title,
             image,
             body,
             tagsArray,
             uid: user.uid,
             createBy: user.displayName,
-        });
+        };
 
-        // redirect to home page
-        navigate("/")
+        updateDocument(id, data);
+
+        // redirect to dashboard page
+        navigate("/dashboard");
     };
 
 
